e553dbf5850993ccaffd992f0985a3962f96b9f1
Yoshemith <cv3227@innovaccion.mx>
2021-10-03 16:29:37 -0500

    register validations done!

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const { validationResult } = require('express-validator');
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 const controller = {
 	register: (req, res) => {
         res.render('register');
 	},
+    store: (req, res) => {
+        console.log(req.body);
+        console.log(req.file);
+        const resultValidation = validationResult(req);
+        console.log(resultValidation);
+        if(resultValidation.errors.length > 0){
+            return res.render('register', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }else{
+            let upImage; 
+            if(req.file){
+                upImage = req.file.filename;
+            }else{
+                upImage = 'profile-user.png';
+            }
+            let newUser = {
+                id: users[users.length - 1].id + 1,
+                ...req.body,
+                image: upImage
+            };
+            users.push(newUser);
+            fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+            res.redirect('/login');
+        }
+    },
     login: (req, res) => {
         res.render('login');
 	}
--- routes/main.js
+++ routes/main.js
@@ -3,24 +3,8 @@ const router = express.Router();
 
 const mainController = require('../controllers/mainController');
 
-const { body } = require('express-validator');
-
-const validations = [
-    body('name').isLength({min:1}).withMessage('Debe ingresar un nombre'),
-    body('color').notEmpty().withMessage('Debe elegir un color'),
-    body('email').isEmail().withMessage('Debe ingresar un email valido'),
-    body('age').notEmpty().withMessage('Debe ingresar su edad').bail(),
-    body('age').custom(value => {
-        if(isNaN(value)){
-            throw new Error('El valor ingresado debe ser un numero');
-        }else {
-            return true;
-        }
-    })
-]
-
 router.get('/', mainController.index); 
-router.post('/', validations, mainController.store); 
+
 router.get('/color', mainController.color);
 router.get('/borrar', mainController.borrar)
 
